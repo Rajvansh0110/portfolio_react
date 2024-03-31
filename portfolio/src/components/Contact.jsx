8d9e27dfc2982365f72912d1d97005b264d1cfb5
Rajvansh0110 <ggi2021.2599@ggi.ac.in>
2024-03-31 11:29:33 +0530

    ds
    sd

---
+++ portfolio/src/components/Contact.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaGithubSquare } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="container contact" id="contact">
+        <h1>CONTACT ME</h1>
+        <div
+          className="contact-icon"
+          data-aos="zoom-in-up"
+          data-aos-duration="1000"
+        >
+          <a href="https://www.instagram.com/rajvanshsingh01/" target="_blank" className="items">
+            <FaInstagram className="icons" />
+          </a>
+          <a href="https://www.google.comhttps://www.facebook.com/himanshu.taneja.399" target="_blank" className="items">
+            <CiFacebook className="icons" />
+          </a>
+          <a href="https://www.linkedin.com/in/rajvansh-singh-90672a270/" target="_blank" className="items">
+            <CiLinkedin className="icons" />
+          </a>
+          <a href="https://github.com/Rajvansh0110" target="_blank" className="items">
+            <FaGithubSquare className="icons" />
+          </a>
+          <a
+            href="mailto:himanshusinghtaneja0110@gmail.com"
+            target="_blank"
+            className="items"
+          >
+            <SiGmail className="icons" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
---
+++ portfolio/src/components/Education.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import experience from "./data/experience.json";
+
+const Education = () => {
+  return (
+    <>
+    <br />
+    <br />
+    <br />
+    <br />
+      <div className="container ex" id="education">
+        <h1>EDUCATION</h1>
+        {experience.map((data) => {
+          return (
+            <>
+              <div
+                key={data.id}
+                className="ex-items text-center my-5"
+                data-aos="zoom-in"
+                data-aos-duration="1000"
+              >
+                <div className="left">
+                  <img src={`/assets/${data.imageSrc}`} alt="" />
+                </div>
+                <div className="right">
+                  <h2>{data.College}</h2>
+                  <h4><span style={{ color: "yellow" }}>{data.location}</span></h4>
+                  <h4>{data.Course}</h4>
+                  <h4>
+                    <span style={{ color: "yellowgreen" }}>
+                      {data.startyear} - {data.endyear}{" "}
+                      <br />
+                    </span>
+                  </h4>
+                  <h5 style={{ color: "yellow" }}>{data.Percentage}</h5>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Education;
